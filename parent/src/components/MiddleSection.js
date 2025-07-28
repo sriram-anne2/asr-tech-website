52da9943d12b0c4b58964b15f1a62f468a8daea9
Sriram Anne <sriram.anne2@gmail.com>
2025-07-28 16:35:08 -0500

    cleanup and added mythri

--- parent/src/components/MiddleSection.js
+++ parent/src/components/MiddleSection.js
@@ -8,10 +8,10 @@ const MiddleSection = () => {
       <div className="projects">
         <div className="project-card">
           <img src={`${process.env.PUBLIC_URL}/economist-splash.png`} alt="Economist App" />
-          <h3>Economist Mobile</h3>
+          <h3>Economist App</h3>
           <p>Personal finance and credit card optimization platform.</p>
           <a
-            href="https://srirams-mac-mini.tail14d518.ts.net/api/home/hello"
+            href="https://srirams-mac-mini.tail14d518.ts.net/api/api/home/hello"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -20,8 +20,29 @@ const MiddleSection = () => {
         </div>
 
         <div className="project-card">
+          <img src={`${process.env.PUBLIC_URL}/mythri-mandirs-logo.png`} alt="Mythri Mandirs" />
+          <h3>Mythri Mandirs</h3>
+          <p>Crafting Bespoke Wooden Mandirams</p>
+          <a
+            href="https://mythri-mandirs.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit us Now!
+          </a>
+        </div>
+
+
+        <div className="project-card">
           <h3>Mythri Property Management</h3>
           <p>Simplified and intuitive platform for both landlords and tenants</p>
+          <a
+            href="https://www.asrtech.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coming soon!
+          </a>
         </div>
       </div>
     </section>
